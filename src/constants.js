b4ee82a20b1ec7a02d5f97b121083ce1bddf1a8e
trebektas <trensarbektas@gmail.com>
2022-11-05 16:51:03 +0100

    add footer contents

--- src/constants.js
+++ src/constants.js
@@ -3,4 +3,4 @@
 export const FETCH_NBA_TEAMS = 'https://www.balldontlie.io/api/v1/teams';
 export const MAIN_CONTENT_ID = 'main-content';
 export const NBA_LOGO_ID = 'nba-logo';
-export const HOME_ID = 'link-home';
+export const HOME_LINK_ID = 'link-home';
